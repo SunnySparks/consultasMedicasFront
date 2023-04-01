51198eea6a4c19795634e6550669c56cfc5acf4f
José Francisco Silva Díaz <Josediazt6@outlook.com>
2023-03-31 18:41:15 -0600

    Created project structure

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Consultas Medicas`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
